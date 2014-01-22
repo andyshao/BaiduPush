6c32a4b3ceafbcfe29a497fc2e283f721867c99b
Huod <huoda2008@163.com>
2014-01-22 14:58:47 +0800

    201401221458

--- BaiduPush/WebService_Push.asmx.cs
+++ BaiduPush/WebService_Push.asmx.cs
@@ -9,7 +9,7 @@ namespace BaiduPushWebService
     /// <summary>
     /// WebService_Push 的摘要说明
     /// </summary>
-    [WebService(Namespace = "http://www.sinobpo.com/")]
+    [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消对下行的注释。
